f2fb5ddeb837670dd508625fdba02c9ce2ac03d0
tishan-harischandra <tishan.harischandra@gmail.com>
2025-07-23 15:26:44 +0530

    Added oracle support for dolphinscheduler metaDB

---
+++ dolphinscheduler-dao-plugin/dolphinscheduler-dao-plugin-oracle/src/main/java/org/apache/dolphinscheduler/dao/plugin/oracle/dialect/OracleDialect.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dolphinscheduler.dao.plugin.oracle.dialect;
+
+import org.apache.dolphinscheduler.dao.plugin.api.dialect.DatabaseDialect;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.sql.DataSource;
+
+import lombok.SneakyThrows;
+
+public class OracleDialect implements DatabaseDialect {
+
+    private final DataSource dataSource;
+
+    public OracleDialect(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @SneakyThrows
+    @Override
+    public boolean tableExists(String tableName) {
+        try (
+                Connection conn = dataSource.getConnection();
+                ResultSet rs = conn.getMetaData().getTables(null, getCurrentSchema(conn), tableName.toUpperCase(),
+                        new String[]{"TABLE"})) {
+            return rs.next();
+        }
+    }
+
+    @SneakyThrows
+    @Override
+    public boolean columnExists(String tableName, String columnName) {
+        try (
+                Connection conn = dataSource.getConnection();
+                ResultSet rs = conn.getMetaData().getColumns(null, getCurrentSchema(conn), tableName.toUpperCase(),
+                        columnName.toUpperCase())) {
+            return rs.next();
+        }
+    }
+
+    @SneakyThrows
+    private String getCurrentSchema(Connection conn) {
+        try (
+                PreparedStatement pstmt =
+                        conn.prepareStatement("SELECT SYS_CONTEXT('USERENV', 'CURRENT_SCHEMA') FROM DUAL");
+                ResultSet resultSet = pstmt.executeQuery()) {
+            if (resultSet.next()) {
+                return resultSet.getString(1);
+            }
+        }
+        return conn.getMetaData().getUserName();
+    }
+}
---
+++ dolphinscheduler-dao-plugin/dolphinscheduler-dao-plugin-oracle/src/main/java/org/apache/dolphinscheduler/dao/plugin/oracle/monitor/OracleMonitor.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dolphinscheduler.dao.plugin.oracle.monitor;
+
+import org.apache.dolphinscheduler.dao.plugin.api.monitor.DatabaseMetrics;
+import org.apache.dolphinscheduler.dao.plugin.api.monitor.DatabaseMonitor;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Date;
+
+import javax.sql.DataSource;
+
+import lombok.SneakyThrows;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+
+public class OracleMonitor implements DatabaseMonitor {
+
+    private final DataSource dataSource;
+
+    public OracleMonitor(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @SneakyThrows
+    @Override
+    public DatabaseMetrics getDatabaseMetrics() {
+        DatabaseMetrics monitorRecord = new DatabaseMetrics();
+        monitorRecord.setDate(new Date());
+        monitorRecord.setDbType(DbType.ORACLE_12C);
+        monitorRecord.setState(DatabaseMetrics.DatabaseHealthStatus.YES);
+
+        try (
+                Connection connection = dataSource.getConnection();
+                Statement pstmt = connection.createStatement()) {
+
+            // Get maximum allowed sessions from V$RESOURCE_LIMIT
+            try (ResultSet rs1 = pstmt.executeQuery(
+                    "SELECT CURRENT_UTILIZATION, MAX_UTILIZATION, LIMIT_VALUE " +
+                    "FROM V$RESOURCE_LIMIT " +
+                    "WHERE RESOURCE_NAME = 'sessions'")) {
+                if (rs1.next()) {
+                    // Set max connections to the limit value
+                    long limitValue = rs1.getLong("LIMIT_VALUE");
+                    monitorRecord.setMaxConnections(limitValue);
+
+                    // Set max used connections to the max utilization
+                    long maxUtilization = rs1.getLong("MAX_UTILIZATION");
+                    monitorRecord.setMaxUsedConnections(maxUtilization);
+                }
+            }
+
+            // Get current number of sessions from V$SESSION
+            try (ResultSet rs2 = pstmt.executeQuery(
+                    "SELECT COUNT(*) AS session_count FROM V$SESSION")) {
+                if (rs2.next()) {
+                    monitorRecord.setThreadsConnections(rs2.getLong("session_count"));
+                }
+            }
+
+            // Get number of active sessions from V$SESSION
+            try (ResultSet rs3 = pstmt.executeQuery(
+                    "SELECT COUNT(*) AS active_count FROM V$SESSION WHERE STATUS = 'ACTIVE'")) {
+                if (rs3.next()) {
+                    monitorRecord.setThreadsRunningConnections(rs3.getLong("active_count"));
+                }
+            }
+        }
+        return monitorRecord;
+    }
+}
